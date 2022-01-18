a42464e1e6081a9562b3e32fada1c97f4827fce3
Nimatullah Razmjo <nimat.razmjo@gmail.com>
2022-01-18 14:03:24 +0430

    Add user entity

---
+++ src/entity/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({
+    unique: true
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  is_ambassador: boolean;
+}
