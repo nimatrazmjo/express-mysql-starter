0cfd660e7d6c5b7f5a854b661f72a65aeeb56238
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-01-29 10:33:22 +0430

    Add seeder for user

---
+++ src/seeders/user.seeder.ts
@@ -0,0 +1,23 @@
+
+import {createConnection, getRepository} from "typeorm";
+import {User} from "../entity/user.entity";
+import bcryptjs from "bcryptjs";
+import faker from "@faker-js/faker";
+
+createConnection().then(async () => {
+    const repository = getRepository(User);
+
+    const password = await bcryptjs.hash("1234", 10);
+
+    for (let i = 0; i < 30; i++) {
+        await repository.save({
+            first_name: faker.name.firstName(),
+            last_name: faker.name.lastName(),
+            email: faker.internet.email(),
+            password,
+            is_ambassador: true
+        })
+    }
+
+    process.exit();
+});
